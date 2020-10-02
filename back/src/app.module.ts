b8163c6cc034895da46b0b7027b89cb287fc79a3
nekroment <adamcevs@gmail.com>
2020-10-02 19:29:16 +0300

    add connection to MongoDB

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { HeroesModule } from './heroes/heroes.module';
+require('dotenv/config');
+
+const connectionString = `mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@cluster0.zkmbj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(connectionString), HeroesModule],
   controllers: [],
   providers: [],
 })
