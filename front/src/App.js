e34d13ff9a0390a1c9a0338cbb32eca07b002a84
nekroment <adamcevs@gmail.com>
2020-10-03 13:24:13 +0300

    add Reducer

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getHeroThunkCreator, changeHeroThunkCreator, deleteHeroThunkCreator } from './redux/reducer/heroReducer';
+import HeroManager from './components/HeroManager';
 
-function App() {
+function App(props) {
+
+  useEffect(() => { return props.getHeroes() }, []);
+
   return (
     <div className="App">
-      
+      <HeroManager heroes={props.heroes} form={props.form}
+        changeHero={props.changeHero} deleteHero={props.deleteHero} />
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  heroes: state.hero.heroes,
+  form: state.hero.form,
+  isLoading: state.hero.isLoading
+});
+
+export default connect(mapStateToProps, {
+  getHeroes: getHeroThunkCreator,
+  changeHero: changeHeroThunkCreator,
+  deleteHero: deleteHeroThunkCreator
+})(App);
