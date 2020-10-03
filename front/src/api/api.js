2d36c437d465683057f6dfa86b26a3b91a9ce5c0
nekroment <adamcevs@gmail.com>
2020-10-03 10:15:17 +0300

    add api.js

---
+++ front/src/api/api.js
@@ -0,0 +1,36 @@
+import * as axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'http://localhost:3000'
+});
+
+export const heroAPI = {
+    async getHeroes () {
+        return await instance.get('/');
+    },
+    async getImage (image) {
+        return await instance.get(`/${image}/image`);
+    },
+    async saveHero (image, hero) {
+        const formData = new FormData();
+        formData.append('file', image);
+        formData.append('nickname', hero.nickname);
+        formData.append('real_name', hero.real_name);
+        formData.append('origin_description', hero.origin_description);
+        formData.append('catch_phrase', hero.catch_phrase);
+        return await instance.post('/', formData);
+    },
+    async changeHero (image, hero) {
+        const formData = new FormData();
+        formData.append('file', image);
+        formData.append('nickname', hero.nickname);
+        formData.append('real_name', hero.real_name);
+        formData.append('origin_description', hero.origin_description);
+        formData.append('catch_phrase', hero.catch_phrase);
+        formData.append('_id', hero._id);
+        return await instance.post('/change', formData);
+    },
+    async deleteHero (_id, image) {
+        return await instance.delete(`/${_id}/${image}`);
+    }
+}
